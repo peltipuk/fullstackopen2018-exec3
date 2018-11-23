57143c2af9953527d12d8dbc5d148586c20166bb
Lauri Vanhatalo <lauri.vanhatalo@nokia.com>
2018-11-23 11:25:06 +0200

    Exercise 3.4

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
 app.get('/info', (req, res) => {
   res.send(`
     <div>puhelinluettelossa ${persons.length} henkilön tiedot</div>
